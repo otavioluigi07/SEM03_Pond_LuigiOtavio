d0dc1b08ba5e4e8bb2fd23e59d154d970fc4960b
otavioluigi07 <luigi.macedo@sou.inteli.edu.br>
2025-05-11 21:37:45 -0300

    feat: final set tup with husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
+import jest from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest, 
+      },
+    },
+    plugins: { jest }, 
+  },
 ]);
